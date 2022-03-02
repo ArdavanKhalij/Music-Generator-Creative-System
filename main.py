d9d4bdc92fcb7de426d3e0bcf9b831112dafa498
Ardavan Khalij <44583966+ArdavanKhalij@users.noreply.github.com>
2022-03-02 20:45:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+#############################################################################
+# Libraries
+#############################################################################
+import random
+import scipy
+from scipy.io.wavfile import read as wavread
+import time
+import numpy as np
+import math
+#############################################################################
 # import wave
 # w_one = wave.open('01 Up and Down.wav', 'r')
 # w_two = wave.open('02 Up and Down (Piano).wav', 'r')
@@ -10,10 +20,10 @@
 # else:
 #     print('not a match')
 
-# [samplerate, y] = wavread('Glow.wav')
-# [samplerate, z] = wavread('02 Up and Down (Piano).wav')
-# print(len(y), len(z))
-#
+# [samplerate, y] = wavread('despair2.wav')
+# [samplerate, z] = wavread('upanddown1.wav')
+# print(samplerate, len(y)/samplerate, len(z)/samplerate)
+
 # start = time.time()
 # w = []
 #
@@ -34,18 +44,6 @@
 
 
 #############################################################################
-# Libraries
-#############################################################################
-import random
-import scipy
-from scipy.io.wavfile import read as wavread
-import time
-import numpy as np
-#############################################################################
-
-
-
-#############################################################################
 # General info of algorithm
 #############################################################################
 # Number of primary population
@@ -57,7 +55,78 @@ MutationProbability = 0.1
 # Sample rate
 SampleRate = 44100
 # Length of the song in seconds
-SongLength = 6
+SongLength = 5.94
+# Song 1 Name
+SongName1 = 'upanddown1.wav'
+# Song 2 Name
+SongName2 = 'upanddown2.wav'
+# Song 3 Name
+SongName3 = 'upanddown3.wav'
+# Song 4 Name
+SongName4 = 'upanddown4.wav'
+# Song 5 Name
+SongName5 = 'upanddown5.wav'
+# Song 6 Name
+SongName6 = 'despair2.wav'
+# Song 7 Name
+SongName7 = 'despair3.wav'
+# Domain
+Domain = 32768 + 32767
+#############################################################################
+
+
+
+#############################################################################
+# Song Decoder
+#############################################################################
+def SongDecoder(SongName):
+    DataOfSong = []
+    [_, y] = wavread(SongName)
+    for i in range(0, int(SongLength*SampleRate)):
+        y1, y2 = [y[i][0], y[i][1]]
+        res = int((y1 + y2) / 2)
+        DataOfSong.append(res)
+    return DataOfSong
+#############################################################################
+
+
+
+#############################################################################
+# Data of Songs
+#############################################################################
+SongData1 = SongDecoder(SongName1)
+SongData2 = SongDecoder(SongName2)
+SongData3 = SongDecoder(SongName3)
+SongData4 = SongDecoder(SongName4)
+SongData5 = SongDecoder(SongName5)
+SongData6 = SongDecoder(SongName6)
+SongData7 = SongDecoder(SongName7)
+#############################################################################
+
+
+
+#############################################################################
+# Find the  local minimum and maximum
+#############################################################################
+domainOfAllSongs = []
+domainOfAllSongs.append(min(SongData1))
+domainOfAllSongs.append(max(SongData1))
+domainOfAllSongs.append(min(SongData2))
+domainOfAllSongs.append(max(SongData2))
+domainOfAllSongs.append(min(SongData3))
+domainOfAllSongs.append(max(SongData3))
+domainOfAllSongs.append(min(SongData4))
+domainOfAllSongs.append(max(SongData4))
+domainOfAllSongs.append(min(SongData5))
+domainOfAllSongs.append(max(SongData5))
+domainOfAllSongs.append(min(SongData6))
+domainOfAllSongs.append(max(SongData6))
+domainOfAllSongs.append(min(SongData7))
+domainOfAllSongs.append(max(SongData7))
+MinOfDomain = min(domainOfAllSongs)
+MaxOfDomain = max(domainOfAllSongs)
+Domain = MaxOfDomain - MinOfDomain
+print(MinOfDomain, MaxOfDomain, Domain)
 #############################################################################
 
 
@@ -65,12 +134,29 @@ SongLength = 6
 #############################################################################
 # Random Primary Population
 #############################################################################
+print("-----------------------------------------")
+print("Primary Population")
+print("-----------------------------------------")
 PrimaryPopulation = []
 for i in range(0, NumberOfPrimaryPopulation):
     PrimaryPopulation.append([])
     print(i)
-    for _ in range(0, SongLength*SampleRate):
-        PrimaryPopulation[i].append([random.randint(-32768, 32767), random.randint(-32768, 32767)])
+    for _ in range(0, int(SongLength*SampleRate)):
+        PrimaryPopulation[i].append(random.randint(MinOfDomain, MaxOfDomain))
+#############################################################################
+
+
+
+#############################################################################
+# Percent of similarity of two songs
+#############################################################################
+def SimilarityOfSongs(DataOfSong1, DataOfSong2):
+    SimilarityScore = []
+    # 1-(DataOfSong1[i][0] - DataOfSong2[i][0])/Domain
+    for i in range(0, int(SongLength*SampleRate)):
+        res = abs(DataOfSong1[i] - DataOfSong2[i])
+        SimilarityScore.append(1 - res / Domain)
+    return sum(SimilarityScore)/len(SimilarityScore)
 #############################################################################
 
 
@@ -78,8 +164,23 @@ for i in range(0, NumberOfPrimaryPopulation):
 #############################################################################
 # Fitness function
 #############################################################################
-def Fitness():
-    pass
+def Fitness(Population):
+    print("-----------------------------------------")
+    print("Fitness Function")
+    print("-----------------------------------------")
+    Scores = []
+    res = 0
+    for i in range(0, NumberOfPrimaryPopulation):
+        res = res + SimilarityOfSongs(SongData1, Population[i])
+        res = res + SimilarityOfSongs(SongData2, Population[i])
+        res = res + SimilarityOfSongs(SongData3, Population[i])
+        res = res + SimilarityOfSongs(SongData4, Population[i])
+        res = res + SimilarityOfSongs(SongData5, Population[i])
+        res = res + SimilarityOfSongs(SongData6, Population[i])
+        res = res + SimilarityOfSongs(SongData7, Population[i])
+        Scores.append(res/7)
+        res = 0
+    return Scores
 #############################################################################
 
 
@@ -87,8 +188,19 @@ def Fitness():
 #############################################################################
 # Roulette wheel mutation
 #############################################################################
-def RouletteWheelMutation():
-    pass
+def RouletteWheelMutation(Population):
+    print("-----------------------------------------")
+    print("Roulette Wheel Mutation")
+    print("-----------------------------------------")
+    Scores = Fitness(Population)
+    ScoreSum = sum(Scores)
+    NormalizedScores = []
+    for i in range(0, NumberOfPrimaryPopulation):
+        NormalizedScores.append(Scores[i]/ScoreSum)
+    res = 1 - sum(NormalizedScores)
+    index = len(NormalizedScores)-1
+    NormalizedScores[index] = NormalizedScores[index] + res
+    return NormalizedScores
 #############################################################################
 
 
@@ -99,3 +211,15 @@ def RouletteWheelMutation():
 def Crossover():
     pass
 #############################################################################
+
+
+
+#############################################################################
+# Test
+#############################################################################
+print(SimilarityOfSongs(SongData1, SongData2))
+RouletteWheelMutation(PrimaryPopulation)
+#############################################################################
+
+# heuristic to see the similarity of learning data and choose base on that to give songs value with fuzzy logic
+# find max and min of learning songs and learn and generat primary population based on that.
